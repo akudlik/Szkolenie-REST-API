cbc77ccf4c03a2a242209b224809ed00a0ff945b
Adrian K <adriankrolik115@gmail.com>
2020-01-18 21:00:24 +0100

    Create project .NET Core 2.2

--- REST.API/Controllers/ValuesController.cs
+++ REST.API/Controllers/ValuesController.cs
@@ -14,7 +14,7 @@ namespace REST.API.Controllers
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
         {
-            return new string[] { "value1", "value2" };
+            return new string[] {"value1", "value2"};
         }
 
         // GET api/values/5
