6e2b48166bd62b384999c0d63028ec2ea3563e48
Adrian K <adriankrolik115@gmail.com>
2020-01-19 16:00:50 +0100

    Sort

--- REST.API/Controllers/UsersController.cs
+++ REST.API/Controllers/UsersController.cs
@@ -1,8 +1,11 @@
-﻿using System;
-using System.Collections;
+﻿using System.Data;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
+using System.Linq.Dynamic;
+using System.Linq.Expressions;
+using System.Reflection;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Primitives;
 using REST.API.Controllers.Model;
@@ -33,11 +36,38 @@ namespace REST.API.Controllers
         {
             if (!ModelState.IsValid)
                 return BadRequest(ModelState);
+
+
+            _usersList=  _usersList.AsQueryable().OrderBy(userFilter.SortBy.Split(","));
+
+
             var result = _usersList.Skip(userFilter.PageNumber * userFilter.PageSize).Take(userFilter.PageSize).ToList();
 
             Response.Headers.Add(new KeyValuePair<string, StringValues>("Count", _usersList.Count().ToString()));
 
             return Ok(result);
+        }
+    }
+
+    public static class QueryableExtensions
+    {
+        public static IQueryable<T> OrderBy<T>(this IQueryable<T> source, IEnumerable<string> sortModels)
+        {
+            var expression = source.Expression;
+            int count = 0;
+            foreach (var item in sortModels)
+            {
+                var parameter = Expression.Parameter(typeof(T), "x");
+                var selector = Expression.PropertyOrField(parameter, item.Replace("-",""));
+                var method = item.Contains("-", StringComparison.OrdinalIgnoreCase) ?
+                    (count == 0 ? "OrderByDescending" : "ThenByDescending") :
+                    (count == 0 ? "OrderBy" : "ThenBy");
+                expression = Expression.Call(typeof(Queryable), method,
+                    new Type[] { source.ElementType, selector.Type },
+                    expression, Expression.Quote(Expression.Lambda(selector, parameter)));
+                count++;
+            }
+            return count > 0 ? source.Provider.CreateQuery<T>(expression) : source;
         }
     }
 
@@ -58,5 +88,21 @@ namespace REST.API.Controllers
         /// </summary>
         [Required]
         public int PageNumber { get; set; }
+
+        /// <summary>
+        /// Number of page
+        /// </summary>
+        public string SortBy { get; set; }
+
+        public static IQueryable<T> OrderByPropertyName<T>(IQueryable<T> q, string SortField, bool Ascending)
+        {
+            var param = Expression.Parameter(typeof(T), "p");
+            var prop = Expression.Property(param, SortField);
+            var exp = Expression.Lambda(prop, param);
+            string method = Ascending ? "OrderBy" : "OrderByDescending";
+            Type[] types = new Type[] {q.ElementType, exp.Body.Type};
+            var rs = Expression.Call(typeof(Queryable), method, types, q.Expression, exp);
+            return q.Provider.CreateQuery<T>(rs);
+        }
     }
 }
